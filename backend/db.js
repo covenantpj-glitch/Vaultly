9566e5b36d5799136f82015fb5de3fbaaad9939c
covenantpj-glitch <covenantpj@gmail.com>
2026-03-21 10:50:44 +0200

    Connect to cloud database

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2');
 
 const db = mysql2.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root123',
-  database: 'budget_planner'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
