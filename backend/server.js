b963f91b257d6214e6d9958ef8dd85cbcd29305f
covenantpj-glitch <covenantpj@gmail.com>
2026-03-21 11:06:07 +0200

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const authRoutes = require('./routes/auth');
 const transactionRoutes = require('./routes/transactions');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
